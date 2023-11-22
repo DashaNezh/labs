e23746ef6a8ac76b3e85acc601cd1e118e433909
DashaNezh <145099798+DashaNezh@users.noreply.github.com>
2023-11-22 18:08:16 +0500

    Update t6.c

--- lab2/t6.c
+++ lab2/t6.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 int MAXSIZE = 8;
-int stack[8];
+int stack[8]; //хранение стека в памяти
 int top = -1;
 
-int isempty(){
+int isempty(){ // проверяет пуст ли стек
    if(top == -1)
       return 1;
    else
       return 0;
 }
 
-int isfull(){
+int isfull(){ // проверяет заполнен ли стек
    if(top == MAXSIZE)
       return 1;
    else
       return 0;
 }
 
-int peek(){
+int peek(){ // возвращает верхний элемент стека
    return stack[top];
 }
 
-int pop(){
+int pop(){ // возвращает элементы из стека
    int data;
    if(!isempty()) {
       data = stack[top];
@@ -32,7 +32,7 @@ int pop(){
    }
 }
 
-int push(int data){
+int push(int data){ //добавляет элементы в стек
    if(!isfull()) {
       top = top + 1;
       stack[top] = data;
@@ -56,23 +56,23 @@ int main(){
    push(673);
    push(7777);
    push(83421);
-   printf("Element at top of the stack: %d\n" ,peek());
+   printf("Element at top of the stack: %d\n" ,peek()); //выводит верхний элемент
    printf("Elements: \n");
-   while(!isempty()) {
+   while(!isempty()) { //пока стек не заполнен выводит эмененты на экран
       int data = pop();
       printf("%d\n",data);
    }
    if (isfull()){
-    printf("Stack is full\n");
+    printf("Stack is full\n"); //сообщение о заполненном стеке
    }
    else {
-    printf("Stack isn't full\n");
+    printf("Stack isn't full\n"); // сообщение о не заполненном стеке
    }
    if (isempty()){
-    printf("Stack is empty\n");
+    printf("Stack is empty\n"); // сообщение о пустом стеке
    }
    else {
-    printf("Stack isn't empty\n");
+    printf("Stack isn't empty\n"); // сообщение о не пустом стеке
    }
    return 0;
 }
